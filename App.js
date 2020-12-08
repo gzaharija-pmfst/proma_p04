ff23220705d37e24e8c92d49de126504647d320c
gzaharija <gzaharija@pmfst.hr>
2020-12-08 09:16:14 +0100

    Pocetak

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
+  const [unos, promjenaUnosa] = useState('')
+  const [ciljevi, postaviCiljeve] = useState([])
+
+  const noviUnos = (tekst) => {
+    promjenaUnosa(tekst)
+  }
+  const noviCilj = () => {
+    console.log(unos)
+    postaviCiljeve(ciljevi => [...ciljevi, unos]);
+    promjenaUnosa('')
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={stilovi.ekran}>
+      <View style={stilovi.viewUnos}>
+        <TextInput
+          placeholder="dodaj cilj"
+          style={stilovi.unos}
+          value={unos}
+          onChangeText={noviUnos}
+        />
+        <Button title="Dodaj"
+          onPress={noviCilj} />
+      </View>
+      <ScrollView>
+        {ciljevi.map(cilj =>
+          <View key={cilj} style={stilovi.lista}>
+            <Text >{cilj}</Text>
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const stilovi = StyleSheet.create({
+  ekran: {
+    padding: 50
   },
-});
+  unos: {
+    width: '70%',
+    marginBottom: 5,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1
+  },
+  viewUnos: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  lista: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+})
